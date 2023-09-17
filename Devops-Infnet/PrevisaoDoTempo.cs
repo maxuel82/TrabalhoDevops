92869147e32785e694a53ff0fb18c3c58ac7162d
maxuel82 <maxuel82@gmail.com>
2023-09-17 00:02:56 -0300

    Removendo atributo TemperaturaF nao utilizado

--- Devops-Infnet/PrevisaoDoTempo.cs
+++ Devops-Infnet/PrevisaoDoTempo.cs
@@ -2,18 +2,17 @@ namespace Devops_Infnet
 {
     public class PrevisaoDoTempo
     {
-        private const int ConstTemperaturaMinimaF = 32;  
+        private const int _constTemperaturaMinimaF = 32;
+        
         public DateTime Data { get; set; }
 
         public int TemperaturaC { get; set; }
-
-        public int TemperaturaF { get { return CaculoTemperaturaF(TemperaturaC);} }
 
         public string? Resumo { get; set; }
 
         public int CaculoTemperaturaF(int temperaturaC)
         {
-            return ConstTemperaturaMinimaF + (int)(temperaturaC / 0.5556);
+            return _constTemperaturaMinimaF + (int)(temperaturaC / 0.5556);
         }
 
     }
