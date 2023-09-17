817ef75b73b63462db673f34e77846c2bf9113d3
maxuel82 <maxuel82@gmail.com>
2023-09-16 23:43:36 -0300

    Subindo segundo teste unitario GetPrevisaoDoTempoComFalha

--- Devops-Infnet/PrevisaoDoTempo.cs
+++ Devops-Infnet/PrevisaoDoTempo.cs
@@ -2,12 +2,19 @@ namespace Devops_Infnet
 {
     public class PrevisaoDoTempo
     {
+        private const int ConstTemperaturaMinimaF = 32;  
         public DateTime Data { get; set; }
 
         public int TemperaturaC { get; set; }
 
-        public int TemperaturaF => 32 + (int)(TemperaturaC / 0.5556);
+        public int TemperaturaF { get { return CaculoTemperaturaF(TemperaturaC);} }
 
         public string? Resumo { get; set; }
+
+        public int CaculoTemperaturaF(int temperaturaC)
+        {
+            return ConstTemperaturaMinimaF + (int)(temperaturaC / 0.5556);
+        }
+
     }
 }
